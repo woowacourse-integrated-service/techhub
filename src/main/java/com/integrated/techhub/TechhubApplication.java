de60f18fcc47d4bd15945e7729073043e4ce922b
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-09-15 19:44:25 +0900

    settings: 프로젝트 세팅

---
+++ src/main/java/com/integrated/techhub/TechhubApplication.java
@@ -0,0 +1,13 @@
+package com.integrated.techhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechhubApplication.class, args);
+	}
+
+}
