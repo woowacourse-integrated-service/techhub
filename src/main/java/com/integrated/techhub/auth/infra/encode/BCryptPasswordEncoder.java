853ea80fd8603c8116063558c3ad101f0c43be28
parkmuhyeun <pjhg410@gmail.com>
2023-09-17 19:53:54 +0900

    test: bcrypt 의존성 추가

--- src/main/java/com/integrated/techhub/auth/infra/encode/BCryptPasswordEncoder.java
+++ src/main/java/com/integrated/techhub/auth/infra/encode/BCryptPasswordEncoder.java
@@ -1,7 +1,7 @@
 package com.integrated.techhub.auth.infra.encode;
 
 import com.integrated.techhub.auth.domain.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
 @Component
