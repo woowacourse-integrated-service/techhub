ff495b3a1d1b94e168fabbd68bb16b2a77c16b6a
parkmuhyeun <pjhg410@gmail.com>
2023-09-17 17:55:42 +0900

    chore: 내장 레디스 설정 추가

--- src/test/java/com/integrated/techhub/mail/infra/mail/TestTemplateEngineWrapper.java
+++ src/test/java/com/integrated/techhub/mail/infra/mail/TestTemplateEngineWrapper.java
@@ -1,10 +1,9 @@
 package com.integrated.techhub.mail.infra.mail;
 
 import com.integrated.techhub.mail.domain.ITemplateEngine;
-import org.springframework.stereotype.Component;
 import org.thymeleaf.context.IContext;
 
-@Component
+
 public class TestTemplateEngineWrapper implements ITemplateEngine {
 
     @Override
