2865033974620a77fb9b52364016ac1534f8b4d5
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-08 16:58:54 +0900

    fix: 여러 단계를 한 번에 제출한 유저의 PR이 한 개만 생성되는 문제

--- src/main/java/com/integrated/techhub/pr/application/PullRequestService.java
+++ src/main/java/com/integrated/techhub/pr/application/PullRequestService.java
@@ -53,7 +53,11 @@ public class PullRequestService {
 
     private void isNotExistSave(final List<PullRequest> newPrs) {
         for (PullRequest newPr : newPrs) {
-            if (!pullRequestRepository.existsByTitle(newPr.getTitle())) {
+            // 1, 2단계 동시 제출한 유저는 똑같은 제목의 pr이 2개 생성된다.(step(fk)만 다른 레코드)
+            // 그렇기 때문에 stepId를 and 조건으로 걸지 않으면 같은 제목의 2번째 pr이 생성되지 않는다.
+            System.out.println("newPr.getStepId() = " + newPr.getStepId());
+            System.out.println("newPr.getTitle() = " + newPr.getTitle());
+            if (!pullRequestRepository.existsByStepIdAndTitle(newPr.getStepId(), newPr.getTitle())) {
                 pullRequestRepository.save(newPr);
             }
         }
