633d0f674336ca4c946f345e13aabb1a2841ee08
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-11-09 21:11:30 +0900

    feat: SSEConnectionRefused 예외 생성

---
+++ src/main/java/com/integrated/techhub/common/sse/exception/SseConnectionRefusedException.java
@@ -0,0 +1,12 @@
+package com.integrated.techhub.common.sse.exception;
+
+import com.integrated.techhub.common.exception.ErrorCode;
+import com.integrated.techhub.common.exception.TechHubException;
+import org.springframework.http.HttpStatus;
+
+public class SseConnectionRefusedException extends TechHubException {
+
+    public SseConnectionRefusedException() {
+        super(new ErrorCode(HttpStatus.BAD_GATEWAY, "SSE 연결 중 오류가 발생했습니다. 다시 시도해주세요."));
+    }
+}
